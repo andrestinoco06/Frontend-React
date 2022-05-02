49046711d5fb906a54c078c1850539d8ec769f3c
andrestinoco06 <kmilo1873@hotmail.com>
2022-05-02 16:52:42 -0500

    Agregando solución al repositorio

---
+++ src/componentes/Item.js
@@ -0,0 +1,14 @@
+// CONSIGNAS EN ITEM:  🚩
+// 1- crear un <li> que tenga su click preparado para ejecutar el metodo heredado para seleccionar la plataforma como favorita
+// 2- Presentar el nombre de la plataforma que le llega como prop
+
+export default function Item(props) {
+    return (
+      <>
+          {props.lista.map((item, index) => {
+              return (<li key={index} onClick={() => props.handleFavorita(item)}>{item}</li>);
+          })}
+      </>
+    );
+  }
+  
